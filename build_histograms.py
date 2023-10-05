e88dfc1ed5f442bec6b11f2beca5accddfea7c98
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2023-10-05 18:51:47 -0500

    Update build_histograms.py
    yeet yah yeet

--- build_histograms.py
+++ build_histograms.py
@@ -3,18 +3,29 @@ import numpy as np
 import numpy as np
 
 def build_histograms(data):
-    """
-    Builds skin and non-skin color histograms from a given dataset file.
-
-    Args:
-    - data (numpy.ndarray): The dataset array with shape (N, 4). Each row represents a pixel, and the 
-                            columns represent the B, G, R values and the label (1 for skin, 2 for non-skin).
-
-    Returns:
-    - skin_histogram (numpy.ndarray): A 3D numpy array representing the skin color histogram.
-    - nonskin_histogram (numpy.ndarray): A 3D numpy array representing the non-skin color histogram.
-    """
- 
-    # Your code here...
-            
-    return skin_histogram, nonskin_histogram
+    skin_histogram = np.zeros((32, 32, 32), dtype=int)
+    non_skin_histogram = np.zeros((32, 32, 32), dtype=int)
+    
+    for pixel in data:
+        b, g, r, label = pixel
+        rgb_pixel = (r, g, b)
+        
+        # skin er not skin bub????
+        if label == 1:
+            histogram = skin_histogram
+        elif label == 2:
+            histogram = non_skin_histogram
+        else:
+            continue  
+        
+        r_bin = int(rgb_pixel[0] * 31 / 255)
+        g_bin = int(rgb_pixel[1] * 31 / 255)
+        b_bin = int(rgb_pixel[2] * 31 / 255)
+        
+        
+        histogram[r_bin, g_bin, b_bin] += 1
+    
+    skin_histogram = skin_histogram / np.sum(skin_histogram)
+    non_skin_histogram = non_skin_histogram / np.sum(non_skin_histogram)
+    
+    return skin_histogram, non_skin_histogram
